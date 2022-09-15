82ed2d2b3a7eca05ad31ff5300a1e6722761e755
Parham Alvani <parham.alvani@gmail.com>
2022-09-16 01:09:57 +0430

    feat: add conference creation and deletion

---
+++ internal/store/conference/sql.go
@@ -0,0 +1,55 @@
+package conference
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aut-cic/backnet/internal/model"
+	"github.com/pterm/pterm"
+	"gorm.io/gorm"
+)
+
+type SQL struct {
+	DB *gorm.DB
+}
+
+func NewSQL(db *gorm.DB) *SQL {
+	return &SQL{
+		DB: db,
+	}
+}
+
+func (sql *SQL) Create(ctx context.Context, name string, count int) ([]model.Check, error) {
+	users := make([]model.Check, count)
+
+	if err := sql.Delete(ctx, name); err != nil {
+		pterm.Error.Printfln("cannot delete the old conference users %s", err)
+	}
+
+	tran := sql.DB.Begin()
+
+	for i := 0; i < count; i++ {
+		users[i] = model.Check{
+			ID:        0,
+			Username:  fmt.Sprintf("%s_%d", name, i),
+			Attribute: "Cleartext-Password",
+			Op:        ":=",
+			Value:     "",
+		}
+
+		tran.WithContext(ctx).Create(&users[i])
+	}
+
+	if err := tran.Commit().Error; err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
+
+func (sql *SQL) Delete(ctx context.Context, name string) error {
+	return sql.DB.
+		WithContext(ctx).
+		Where("username LIKE ?", fmt.Sprintf("%s_%%", name)).
+		Delete(new(model.Check)).Error
+}
