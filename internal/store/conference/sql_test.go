cdb4abd87409a1a6828b9891696168b99d277a4e
Parham Alvani <parham.alvani@gmail.com>
2022-09-25 10:13:21 +0330

    feat: improve sql tests

--- internal/store/conference/sql_test.go
+++ internal/store/conference/sql_test.go
@@ -33,13 +33,18 @@ func (suite *SQLConferenceSuite) SetupSuite() {
 func (suite *SQLConferenceSuite) TearDownSuite() {
 }
 
+func (suite *SQLConferenceSuite) TearDownTest() {
+	suite.DB.Delete(new(model.Check))
+	suite.DB.Delete(new(model.UserGroup))
+}
+
 func (suite *SQLConferenceSuite) TestNewConference() {
 	require := suite.Require()
 
 	users, err := suite.Store.Create(context.Background(), "elahe", 10, "Faculty")
 	require.NoError(err)
 
-	require.Equal(10, len(users))
+	require.Len(users, 10)
 
 	require.Equal("elahe00", users[0].Username)
 
@@ -47,6 +52,46 @@ func (suite *SQLConferenceSuite) TestNewConference() {
 
 	require.NoError(suite.DB.Where("username = ?", "elahe09").Find(ug).Error)
 
+	require.Equal("Faculty", ug.Groupname)
+}
+
+func (suite *SQLConferenceSuite) TestListConference() {
+	require := suite.Require()
+
+	_, err := suite.Store.Create(context.Background(), "parham", 10, "Faculty")
+	require.NoError(err)
+
+	suite.DB.Create(model.UserGroup{
+		Username:  "parham.alvani",
+		Groupname: "Faculty",
+		ID:        0,
+	})
+
+	users, err := suite.Store.List(context.Background(), "parham")
+	require.NoError(err)
+
+	suite.Len(users, 10)
+	suite.NotContains(users, "parham.alvani")
+}
+
+func (suite *SQLConferenceSuite) TestDeleteConference() {
+	require := suite.Require()
+
+	_, err := suite.Store.Create(context.Background(), "parham", 10, "Faculty")
+	require.NoError(err)
+
+	suite.DB.Create(model.UserGroup{
+		Username:  "parham.alvani",
+		Groupname: "Faculty",
+		ID:        0,
+	})
+
+	require.NoError(suite.Store.Delete(context.Background(), "parham"))
+
+	ug := new(model.UserGroup)
+	require.NoError(suite.DB.Where("username = parham.alvani").Find(ug).Error)
+
+	require.Equal("parham.alvani", ug.Username)
 	require.Equal("Faculty", ug.Groupname)
 }
 
