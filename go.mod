4b026910b18f5ceb721f3ba0d62e38624458dd67
Parham Alvani <parham.alvani@gmail.com>
2023-03-13 11:05:46 +0330

    feat: add loading spinner

--- go.mod
+++ go.mod
@@ -17,7 +17,9 @@ require (
 	atomicgo.dev/keyboard v0.2.9 // indirect
 	github.com/aymanbagabas/go-osc52 v1.2.2 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/charmbracelet/bubbles v0.15.0 // indirect
 	github.com/charmbracelet/bubbletea v0.23.2 // indirect
+	github.com/charmbracelet/lipgloss v0.7.1 // indirect
 	github.com/containerd/console v1.0.3 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
--- internal/cmd/cli/main.go
+++ internal/cmd/cli/main.go
@@ -8,63 +8,80 @@ import (
 	"github.com/aut-cic/backnet/internal/db"
 	"github.com/aut-cic/backnet/internal/model"
 	"github.com/aut-cic/backnet/internal/store/discount"
+	"github.com/charmbracelet/bubbles/spinner"
+	"github.com/charmbracelet/bubbles/table"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/pterm/pterm"
 )
 
+// nolint gochecknoglobals
+var baseStyle = lipgloss.NewStyle().
+	BorderStyle(lipgloss.NormalBorder()).
+	BorderForeground(lipgloss.Color("240"))
+
 type discountModel struct {
-	discounts []model.Discount
-	store     discount.Discount
-	cursor    int
+	store discount.Discount
+
+	isLoading bool
+
+	table   table.Model
+	spinner spinner.Model
 }
 
-func (m *discountModel) Init() tea.Cmd {
+type discountListMsg struct {
+	discounts []model.Discount
+}
+
+func (m discountModel) List() tea.Msg {
 	discounts, err := m.store.List(context.Background())
 	if err != nil {
-		pterm.Fatal.Printfln("loading discounts failed %s", err)
+		pterm.Fatal.Printfln("reading from database failed %s", err)
 	}
 
-	m.discounts = discounts
-
-	return nil
+	return discountListMsg{
+		discounts: discounts,
+	}
 }
 
-func (m *discountModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	if msg, ok := msg.(tea.KeyMsg); ok {
+func (m discountModel) Init() tea.Cmd {
+	return m.List
+}
+
+func (m discountModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
 		switch msg.String() {
 		case "ctrl+c", "q":
 			return m, tea.Quit
-
-		case "up", "k":
-			if m.cursor > 0 {
-				m.cursor--
-			}
-
-		case "down", "j":
-			if m.cursor < len(m.discounts)-1 {
-				m.cursor++
+		}
+	case discountListMsg:
+		rows := make([]table.Row, len(msg.discounts))
+		for i, discount := range msg.discounts {
+			rows[i] = table.Row{
+				discount.Type(),
+				discount.Value(),
+				fmt.Sprintf("%g", discount.Factor()),
 			}
 		}
+
+		m.isLoading = false
+		m.table.SetRows(rows)
 	}
 
-	return m, nil
+	m.table, cmd = m.table.Update(msg)
+
+	return m, cmd
 }
 
-func (m *discountModel) View() string {
-	s := "AUT Internet discounts\n\n"
-
-	for i, discount := range m.discounts {
-		cursor := " "
-		if m.cursor == i {
-			cursor = ">" // cursor!
-		}
-
-		s += fmt.Sprintf("%s [ ] %s %s %f\n", cursor, discount.Type(), discount.Value(), discount.Factor())
+func (m discountModel) View() string {
+	if m.isLoading {
+		return fmt.Sprintf("\n\n   %s Loading from database...\n\n", m.spinner.View())
 	}
 
-	s += "\nPress q to quit.\n"
-
-	return s
+	return baseStyle.Render(m.table.View()) + "\n"
 }
 
 func Discount() {
@@ -75,13 +92,26 @@ func Discount() {
 		pterm.Fatal.Printfln("database initiation failed %s", err)
 	}
 
-	dm := discountModel{
-		store:     discount.NewSQL(db),
-		discounts: nil,
-		cursor:    0,
+	// nolint: gomnd
+	columns := []table.Column{
+		{Title: "Type", Width: 20},
+		{Title: "Value", Width: 20},
+		{Title: "Factor", Width: 10},
 	}
 
-	p := tea.NewProgram(&dm)
+	// nolint: gomnd
+	dm := discountModel{
+		store:     discount.NewSQL(db),
+		isLoading: true,
+		spinner:   spinner.New(),
+		table: table.New(
+			table.WithColumns(columns),
+			table.WithFocused(true),
+			table.WithHeight(7),
+		),
+	}
+
+	p := tea.NewProgram(dm)
 	if _, err := p.Run(); err != nil {
 		pterm.Fatal.Println("could not start program:", err)
 	}
